4bae7f7cc79f9831425424e0a3aae963fa2072d9
thmsLG <thomas.leguillou@gmail.com>
2013-05-08 07:15:07 -0700

    Add details in commentaries.

--- ObserverSubject.h
+++ ObserverSubject.h
@@ -4,11 +4,20 @@
 #include "DelegateBooleanParameter.h" // to notify observers.
 #include <vector> // to store observers
 
+//--------------------------------------------------------------------------------------------------------
+//! This class is responsible to notify observers, previously subscribed to it.
+//! It's the subject part of the Observer pattern.
+//! This implementation avoid you to inherit a lot of interface. To subscribe to the subject you only
+//! have to subscribe with a particular signature method.
+//!
+//! For example his implementation is more understandable for me when you have to observe a lot of different
+//! Inputs !
 class ObserverSubject {
 public:
 	//--------------------------------------------------------------------------------------------------------
 	//! \pre None
 	//! \post  None
+	//! avoid copy
 	explicit ObserverSubject()
 	{
 	}
@@ -22,11 +31,9 @@ public:
 
 	//--------------------------------------------------------------------------------------------------------
 	//! \pre 'subscriber' NOT NULL pointer to object where 'handler' will be called
-	//!	'handler' is a method pointer from subscriber. This handler has the following signature :
-	//!	void handler(bool newState)
-	//!	this handler will be called when event will occurs onto notifier, .
+	//!	'handler' is a method pointer from subscriber instance.
 	//! \post Object pointer and handler are stored for a futur notifying.
-	void subscribe(DelegateBooleanParameter * const delegateToInvoke);
+	void subscribe(DelegateBooleanParameter * const delegateToInvokeWhenNotifyIsNeeded);
 
 	//--------------------------------------------------------------------------------------------------------
 	//! \pre 'newState' will be transfered to all subscriber
@@ -34,6 +41,8 @@ public:
 	void notifyObserver(bool newState);
 
 private:
+
+	//!	Theses handlers will be called when notifyObserver( newState) will be called .
 	std::vector<DelegateBooleanParameter*> _observerList;
 };
 
